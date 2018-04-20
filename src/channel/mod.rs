68189a882426407f5e5989adf393b715fa4e9a28
Hyeonu Park <nemo1275@gmail.com>
2018-04-20 13:45:40 +0900

    add test for spinning mpmc

--- src/channel/mod.rs
+++ src/channel/mod.rs
@@ -65,11 +65,12 @@ pub fn channel<S, R, E, T>(
 mod tests {
     use super::*;
     use std::thread;
-    use sequence::Owned;
+    use rand::{thread_rng, Rng};
+    use sequence::{Owned, Shared};
 
     #[test]
     fn test_spining_spsc() {
-        const COUNT: u32 = 16000;
+        const COUNT: u32 = 64000;
         let (mut tx, mut rx) = channel::<Owned, Owned, (), u32>(16);
 
         let tx = thread::spawn(move|| {
@@ -95,5 +96,64 @@ mod tests {
 
         tx.join().expect("Sender thread panicked");
         rx.join().expect("Receiver thread panicked");
+    }
+
+    #[test]
+    fn test_spninning_mpmc() {
+        const COUNT: u32 = 2000;
+        let (tx, mut rx) = channel::<Shared, Shared, (), u32>(64);
+
+        let tx_handles: Vec<_> = (0..4).map(|_n| {
+            let mut tx = tx.clone();
+            thread::spawn(move|| {
+                let mut rng = thread_rng();
+                let mut acc = 0u64;
+
+                for _i in 0..COUNT {
+                    loop {
+                        let v: u32 = rng.gen();
+                        if let Ok(()) = tx.try_send(v) {
+                            // println!("sent: {} - {}", _n, _i);
+                            acc += v as u64;
+                            break;
+                        }
+                    }
+                }
+
+                acc
+            })
+        }).collect();
+
+        let rx_handles: Vec<_> = (0..4).map(|_n| {
+            let mut rx = rx.clone();
+            thread::spawn(move|| {
+                let mut acc = 0u64;
+
+                for _i in 0..COUNT {
+                    loop {
+                        if let Ok(v) = rx.try_recv() {
+                            // println!("recv: {} - {}", _n, _i);
+                            acc += v.unwrap() as u64;
+                            break;
+                        }
+                    }
+                }
+
+                acc
+            })
+        }).collect();
+
+        drop(tx);
+
+        let tx_acc: u64 = tx_handles.into_iter()
+            .map(|h| h.join().expect("Sender thread panicked"))
+            .sum();
+
+        let rx_acc: u64 = rx_handles.into_iter()
+            .map(|h| h.join().expect("Receiver thread panicked"))
+            .sum();
+
+        assert_eq!(tx_acc, rx_acc);
+        assert_eq!(rx.try_recv().unwrap(), None);
     }
 }
