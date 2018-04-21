35d6a1460ee5ecbe9bc0e73eeaf776ef9f2bab61
Hyeonu Park <nemo1275@gmail.com>
2018-04-21 18:23:20 +0900

    add test for blocking channel
    and some cosmetics

--- src/blocker/sync.rs
+++ src/blocker/sync.rs
@@ -45,7 +45,15 @@ impl<S: Sequence, R: Sequence, T: Send> Sender<S, R, Blocker, T> {
                         next_cond = WakeReceiver;
                         next_target = Neutral;
                     }
-                    (BlockReceiver, _) |
+
+                    (BlockReceiver, Neutral)      |
+                    (BlockReceiver, BlockSender)  |
+                    (BlockReceiver, WakeSender)   |
+                    (BlockReceiver, WakeReceiver) |
+
+                    (WakeReceiver, Neutral)     |
+                    (WakeReceiver, BlockSender) |
+                    (WakeReceiver, WakeSender)  |
                     (WakeReceiver, WakeReceiver) => break,
 
                     (WakeReceiver, BlockReceiver) => {
@@ -53,11 +61,13 @@ impl<S: Sequence, R: Sequence, T: Send> Sender<S, R, Blocker, T> {
                         next_target = WakeReceiver;
                     }
 
-                    (_, _) => unreachable!(),
+                    (Neutral,     prev @ _) |
+                    (Locked,      prev @ _) |
+                    (BlockSender, prev @ _) |
+                    (WakeSender,  prev @ _) => unreachable!("{:?}", prev),
                 }
             }
 
-            #[inline(always)]
             fn push_current(ext: &Blocker) {
                 ext.senders.push(thread::current());
                 let locked = ext.state.transit(Locked, BlockSender);
@@ -84,18 +94,18 @@ impl<S: Sequence, R: Sequence, T: Send> Sender<S, R, Blocker, T> {
                                 break;
                             }
 
-                            (Neutral, next @ BlockSender) |
-                            (Neutral, next @ WakeSender) |
-                            (BlockSender, next @ Neutral) |
-                            (BlockSender, next @ WakeSender) |
-                            (WakeSender, next @ Neutral) |
-                            (WakeSender, next @ BlockSender) => {
+                            (Neutral,     next @ BlockSender) |
+                            (Neutral,     next @ WakeSender)  |
+                            (BlockSender, next @ Neutral)     |
+                            (BlockSender, next @ WakeSender)  |
+                            (WakeSender,  next @ Neutral)     |
+                            (WakeSender,  next @ BlockSender) => {
                                 next_cond = next;
                             }
 
-                            (Locked, _) |
-                            (BlockReceiver, _) |
-                            (WakeReceiver, _) => unreachable!(),
+                            (Locked,        prev @ _) |
+                            (BlockReceiver, prev @ _) |
+                            (WakeReceiver,  prev @ _) => unreachable!("{:?}", prev),
                         }
                     }
                 }
@@ -122,7 +132,15 @@ impl<S: Sequence, R: Sequence, T: Send> Receiver<S, R, Blocker, T> {
                         next_cond = WakeSender;
                         next_target = Neutral;
                     }
-                    (BlockSender, _) |
+
+                    (BlockSender, Neutral)       |
+                    (BlockSender, BlockReceiver) |
+                    (BlockSender, WakeReceiver)  |
+                    (BlockSender, WakeSender)    |
+
+                    (WakeSender, Neutral)       |
+                    (WakeSender, BlockReceiver) |
+                    (WakeSender, WakeReceiver)  |
                     (WakeSender, WakeSender) => break,
 
                     (WakeSender, BlockSender) => {
@@ -130,11 +148,13 @@ impl<S: Sequence, R: Sequence, T: Send> Receiver<S, R, Blocker, T> {
                         next_target = WakeSender;
                     }
 
-                    (_, _) => unreachable!(),
+                    (Neutral,       prev @ _) |
+                    (Locked,        prev @ _) |
+                    (BlockReceiver, prev @ _) |
+                    (WakeReceiver,  prev @ _) => unreachable!("{:?}", prev),
                 }
             }
 
-            #[inline(always)]
             fn push_current(ext: &Blocker) {
                 ext.receivers.push(thread::current());
                 let locked = ext.state.transit(Locked, BlockReceiver);
@@ -159,22 +179,112 @@ impl<S: Sequence, R: Sequence, T: Send> Receiver<S, R, Blocker, T> {
                                 break;
                             }
 
-                            (Neutral, next @ BlockReceiver) |
-                            (Neutral, next @ WakeReceiver) |
-                            (BlockReceiver, next @ Neutral) |
-                            (BlockReceiver, next @ WakeReceiver) |
-                            (WakeReceiver, next @ Neutral) |
-                            (WakeReceiver, next @ BlockReceiver) => {
+                            (Neutral,       next @ BlockReceiver) |
+                            (Neutral,       next @ WakeReceiver)  |
+                            (BlockReceiver, next @ Neutral)       |
+                            (BlockReceiver, next @ WakeReceiver)  |
+                            (WakeReceiver,  next @ Neutral)       |
+                            (WakeReceiver,  next @ BlockReceiver) => {
                                 next_cond = next;
                             }
 
-                            (Locked, _) |
-                            (BlockSender, _) |
-                            (WakeSender, _) => unreachable!(),
+                            (Locked,      prev @ _) |
+                            (BlockSender, prev @ _) |
+                            (WakeSender,  prev @ _) => unreachable!("{:?}", prev),
                         }
                     }
                 }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::{self, Rng};
+    use sequence::{Owned, Shared};
+    use channel::channel;
+
+    #[test]
+    fn test_blocking_spsc() {
+        const COUNT: u32 = 320000;
+        let (mut tx, mut rx) = channel::<Owned, Owned, Blocker, u32>(64);
+
+        let tx = thread::spawn(move|| {
+            for i in 0..COUNT {
+                tx.send(i);
+                // println!("sent {}", i);
+            }
+        });
+
+        let rx = thread::spawn(move|| {
+            for i in 0..COUNT {
+                assert_eq!(rx.recv(), i);
+                // println!("recv {}", i);
+            }
+        });
+
+        tx.join().expect("sender thread panicked");
+        rx.join().expect("receiver thread panicked");
+    }
+
+    #[test]
+    fn test_blocking_mpmc() {
+        let (tx, rx) = channel::<Shared, Shared, Blocker, u32>(16);
+        const COUNT: u32 = 16000;
+
+        let tx_threads: Vec<_> = (0..8)
+            .map(|_n| {
+                let mut tx = tx.clone();
+                thread::Builder::new()
+                    .name(format!("mpmc-sender-{}", _n))
+                    .spawn(move|| {
+                        let mut rng = rand::thread_rng();
+                        let mut acc = 0u64;
+
+                        for _i in 0..COUNT {
+                            let num = rng.gen_range(0u32, 256);
+                            acc += num as u64;
+                            tx.send(num);
+                            // println!("send {}-{}", _n, _i);
+                        }
+
+                        acc
+                    })
+                    .unwrap()
+            })
+            .collect();
+
+        let rx_threads: Vec<_> = (0..8)
+            .map(|_n| {
+                let mut rx = rx.clone();
+                thread::Builder::new()
+                    .name(format!("mpmc-receiver-{}", _n))
+                    .spawn(move|| {
+                        let mut acc = 0u64;
+
+                        for _i in 0..COUNT {
+                            acc += rx.recv() as u64;
+                            // println!("recv {}-{}", _n, _i);
+                        }
+
+                        acc
+                    })
+                    .unwrap()
+            })
+            .collect();
+
+        drop(tx);
+
+        let tx_sum: u64 = tx_threads.into_iter()
+            .map(|handle| handle.join().expect("sender thread panicked"))
+            .sum();
+
+        let rx_sum: u64 = rx_threads.into_iter()
+            .map(|handle| handle.join().expect("receiver thread panicked"))
+            .sum();
+
+        assert_eq!(tx_sum, rx_sum);
     }
 }
